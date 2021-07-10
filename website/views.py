7e47dbfa852b6d636cb07827d6d277a1acd85522
Vidhi Kumar <vidhikumar2000@gmail.com>
2021-07-10 15:44:45 +0530

    template made,nav done

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 # here we have the pages of our website
 from flask import Blueprint #Represents a blueprint, a collection of routes 
         # and other app-related functions that can be registered on a real application later.
+from flask import render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # below runs the function for this route
 def home():
-    return "<h1>Testing</h1>"
+    return render_template("home.html") # this will render home.html
+
 # we need to register these blueprints in init.py
