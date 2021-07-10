fb84148614291e1c199aed1be273c0012dafc430
Vidhi Kumar <vidhikumar2000@gmail.com>
2021-07-10 12:18:50 +0530

    made a flask app

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,9 @@
 # __init__ will make the website directory a python package
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'cjsd skjcnsd ksjcnsdkjcn' # private key for the app
+
+    return app
