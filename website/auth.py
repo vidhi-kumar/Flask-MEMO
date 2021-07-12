53790814b208cb4c0ac43294352948f37e32ebb3
Vidhi Kumar <vidhikumar2000@gmail.com>
2021-07-12 22:57:30 +0530

    login done

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,10 @@ from flask import Blueprint #Represents a blueprint, a collection of routes
         # and other app-related functions that can be registered on a real application later.
 
 from flask import render_template, request
-from flask import flash
+from flask import flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash #for hashing passw
 
 auth = Blueprint('auth', __name__)
 
@@ -11,6 +14,21 @@ auth = Blueprint('auth', __name__)
 def login():
     # data = request.form
     # print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # checking if that email exists or not
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', category='success')
+            else:
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('Email does not exist', category='error')
+
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -22,17 +40,21 @@ def sign_up():
     # taking data only in post method
     if request.method == "POST":
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        # first we check if same email is not present in the database already
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
         # conditions for data entered(messege flashing)
-        if len(email) < 5:
+        elif len(email) < 5:
             flash('Email must be greater than 4 letters', category='error')
-        elif len(firstName) < 1:
+        elif len(first_name) < 1:
             flash('First name cannot be empty', category='error')
-        elif len(lastName) < 1:
+        elif len(last_name) < 1:
             flash('Last name cannot be empty', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error') # escape char used
@@ -40,6 +62,11 @@ def sign_up():
             flash('Password must contain atleast 5 characters', category='error')
         else:
             # add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
+            #sha56 is a hashing algo
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -4,21 +4,24 @@ from flask_login import UserMixin # some functions to help with user
 from sqlalchemy.sql import func # func can return time
 
 # this is how we create class for objects of database
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # foreign key,'u' us not capital
+                                                  # for foreign key
+    
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
     first_name = db.Column(db.String(128))
     last_name = db.Column(db.String(128))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # foreign key,'u' us not capital
-                                                  # for foreign key
-
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
     notes = db.relationship('Note') # here we make the relationship, capital here
+
+
+
 
 # class Reminder(db.Model)
 # you can have more features!!
