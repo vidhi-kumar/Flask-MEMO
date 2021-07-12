928057c484d5a112278235fc68a4731fad5bf624
Vidhi Kumar <vidhikumar2000@gmail.com>
2021-07-12 20:20:11 +0530

    models done

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,21 @@
 # __init__ will make the website directory a python package
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy # for database
+
+# defining a new database
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'cjsd skjcnsd ksjcnsdkjcn' # private key for the app
+    # time to tell app about our database and its location
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # now we pass app to database
+    db.init_app(app)
+    
 
     # importing views
     from .views import views
